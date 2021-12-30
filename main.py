bed071255c3a76e198d8ff193377b6edfd7f15c0
coding-gene <andricivan86@gmail.com>
2021-12-30 16:01:36 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from logic.twitter import ScrapeTwitterData
+#from logic.twitter import ScrapeTwitterData
 from logic.stock import ScrapeStockData
 from logic.config import get_env_variables
 import logging
 import time
-import sqlite3
-import pandas as pd
 
 try:
-    #conn = sqlite3.connect('stock.db')
     start_time = time.time()
     logging.basicConfig(filename='logs.txt',
                         filemode='a',
@@ -15,20 +12,15 @@ try:
                         level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
     logging.info(f'Pocetak izvrsavanja zadatka.')
-
+    # Initializing local packages
     eVar = get_env_variables()
     #twitter = ScrapeTwitterData(eVar.get('twitter'))
     stock = ScrapeStockData(eVar.get('stock'))
-
     # Twitter data
     #twitter_content = twitter.get_twitter_posts()
     #df_twitter = twitter.get_df(twitter_content)
     # Stock data
     stock_content = stock.save_page_content()
-
-    #df = pd.read_sql_query("SELECT * FROM gme_stock_data", conn)
-    #print(df)
-
 except Exception:
     logging.exception(f'Dogodila se greska sljedeceg sadrzaja:')
     stock.rollback_connection()
