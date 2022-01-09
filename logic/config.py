74323a06148a79a73f396d30e588b09069fe5171
Ivan Andrić <andricivan86@gmail.com>
2022-01-09 19:56:52 +0100

    Initial commit

--- logic/config.py
+++ logic/config.py
@@ -2,6 +2,6 @@ import json
 
 
 def get_env_variables():
-    with open(r'C:\Users\Ivana\PycharmProjects\TwitterSentimentAnalysis\env.json', 'r', encoding="utf-8") as content:
+    with open(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\env.json', 'r', encoding="utf-8") as content:
         env_var_value = json.load(content)
     return env_var_value
